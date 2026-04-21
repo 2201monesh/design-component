6c047991a5137446f1f6070473f41d5af64be0f6
Monesh Goyal <goyalmonesh2201@gmail.com>
2026-04-22 00:38:15 +0530

    design changes for the left section of email product design

--- components/EmailProductDesign.tsx
+++ components/EmailProductDesign.tsx
@@ -2,13 +2,18 @@ import React from 'react'
 import { IoSearchOutline } from "react-icons/io5";
 import SomeoneImage from "../public/someone-text.png"
 import Image from 'next/image';
-
+import { LuInbox } from "react-icons/lu";
+import { VscSend } from "react-icons/vsc";
+import { CiFileOn } from "react-icons/ci";
+import { PiWarningCircle } from "react-icons/pi";
+import { CiStar } from "react-icons/ci";
+import { AiOutlineDelete } from "react-icons/ai";
 
 const EmailProductDesign = () => {
   return (
     <div className='bg-[#ebebeb] h-full w-full flex items-center justify-center'>
       <div className='w-180 h-110 bg-white rounded-tl-4xl'>
-        <div className='header w-full h-[18%] flex items-center pl-6'>
+        <div className='header w-full h-[18%] flex items-center pl-4'>
           {/* <p className='text-[40px] mr-6'>someone</p> */}
           <Image src={SomeoneImage} alt='text' className='mr-5' />
           <div className='w-40 h-10 mt-1 rounded-full bg-[#f7f7f7] flex items-center mr-6'>
@@ -21,15 +26,41 @@ const EmailProductDesign = () => {
           </div>
         </div>
         <div className='w-full h-[82%] flex'>
-          <div className='w-[46%] px-9'>
-            <p>hello</p>
+          <div className='w-[46%] px-7 flex flex-col'>
+            <div className='w-full h-12 rounded-full bg-[#f9f9f9] flex items-center justify-between px-4'>
+              <LuInbox size={22} />
+              <VscSend size={22} />
+              <CiFileOn size={22} />
+              <PiWarningCircle size={22} />
+              <CiStar size={26} />
+              <AiOutlineDelete size={22} />
+            </div>
+            <div className='mt-5 w-full px-4 overflow-y-hidden'>
+              <TextTemplate heading="Arc Studio" date="Today" subHeading="Your website draft is ready" para="We've uploaded the first pass of the homepage. Would love your feedback on..." />
+              <TextTemplate heading="Stripe" date="Tomorrow" subHeading="Your payout has been processed" para="A transfer of $2,480.00 has been sent to your bank account. It should arrive within 1-2 b..." />
+              <TextTemplate heading="Github" date="Yesterday" subHeading="Security alert for one of your repositories" para="We detected  a dependency with a known vulnerability. Review the alert to keep you..." />
+              <TextTemplate heading="Notion" date="Yesterday" subHeading='You were mentioned in "Product Roadmap"' para="We've uploaded the first pass of the homepage. Would love your feedback on lay..." />
+            </div>
           </div>
-          <div className='border w-[54%]'>hiii</div>
+          <div className='border-2 rounded-tl-2xl border-[#f1f1f1] w-[54%]'></div>
         </div>
       </div>
     </div>
   )
 }
 
-export default EmailProductDesign
+export default EmailProductDesign;
+
+const TextTemplate = ({heading, date, subHeading, para}) => {
+  return(
+    <div className='flex flex-col mb-6'>
+      <div className='flex items-center justify-between'>
+        <p className='text-sm font-semibold'>{heading}</p>
+        <p className='text-xs'>{date}</p>
+      </div>
+      <p className='text-xs'>{subHeading}</p>
+      <p className='text-xs'>{para}</p>
+    </div>
+  )
+}
 
