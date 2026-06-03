1c83583beb34f421147f6650270b3bb87dbfc8f5
Monesh Goyal <goyalmonesh2201@gmail.com>
2026-06-03 18:49:03 +0530

    added bg images in card carousal component

--- components/CardCarousal.tsx
+++ components/CardCarousal.tsx
@@ -10,6 +10,15 @@ const GAP_PX = 30 // spacing between adjacent card edges
 const RADIUS = Math.round((CARD_WIDTH_PX + GAP_PX) / (2 * Math.tan(Math.PI / CARD_COUNT))) // ~267px
 const SCROLL_SENSITIVITY = 0.3
 
+const CARD_IMAGES = [
+  'https://i.pinimg.com/736x/d2/f7/48/d2f74855f505e846f70b6fea8328c282.jpg',
+  'https://i.pinimg.com/736x/85/a6/ec/85a6ecb98250e3b9d7a51a341ff30ae4.jpg',
+  'https://i.pinimg.com/736x/2e/19/ec/2e19ecbf70db86240090cb5570c35283.jpg',
+  'https://i.pinimg.com/736x/d7/ab/ab/d7abab99acbb079c9c833ed8b1e9329e.jpg',
+  'https://i.pinimg.com/736x/e5/34/b3/e534b37d96c742e341384f3daf4bef2f.jpg',
+  'https://i.pinimg.com/736x/50/9e/49/509e49a1eede7ac819a3a7e70d452498.jpg',
+]
+
 const CardCarousal = () => {
   const [rotation, setRotation] = useState(0)
   const containerRef = useRef<HTMLDivElement>(null)
@@ -37,8 +46,8 @@ const CardCarousal = () => {
         className="relative w-72 h-34"
         style={{ transformStyle: 'preserve-3d', transform: `rotateY(${rotation}deg)` }}
       >
-        {Array.from({ length: CARD_COUNT }, (_, i) => (
-          <Card key={i} index={i} />
+        {CARD_IMAGES.map((img, i) => (
+          <Card key={i} index={i} image={img} />
         ))}
       </div>
     </div>
@@ -47,12 +56,15 @@ const CardCarousal = () => {
 
 export default CardCarousal
 
-const Card = ({ index }: { index: number }) => {
+const Card = ({ index, image }: { index: number; image: string }) => {
   const rotateY = ANGLE_STEP * index
   return (
     <div
-      className="absolute inset-0 rounded-xl bg-neutral-200"
-      style={{ transform: `rotateY(${rotateY}deg) translateZ(${RADIUS}px)` }}
+      className="absolute inset-0 rounded-xl bg-neutral-200 bg-cover bg-center"
+      style={{
+        transform: `rotateY(${rotateY}deg) translateZ(${RADIUS}px)`,
+        backgroundImage: `url(${image})`,
+      }}
     />
   )
 }
