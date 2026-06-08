97e6ee6e247f1af415add725ec44a606c4907638
Monesh Goyal <goyalmonesh2201@gmail.com>
2026-06-08 21:20:38 +0530

    added first draft ui for the sidebar template design component

--- components/sidebar-template-design.tsx
+++ components/sidebar-template-design.tsx
@@ -1,5 +1,11 @@
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { TbLayoutSidebarFilled } from "react-icons/tb";
+import { LuBrain } from "react-icons/lu";
+import { IoSearch } from "react-icons/io5";
+import { FiUpload } from "react-icons/fi";
+import { PiArrowsClockwise } from "react-icons/pi";
+import { LuCopy } from "react-icons/lu"
+import { HiOutlineDotsHorizontal } from "react-icons/hi";
 
 const SidebarTemplateDesign = () => {
   return (
@@ -36,12 +42,38 @@ const SidebarTemplateDesign = () => {
                             <p className="text-xs text-neutral-500">Updated 2 mins ago</p>
                         </div>
                     </div>
-                    <TbLayoutSidebarFilled size={18} className="text-neutral-500" />
+                    <TbLayoutSidebarFilled size={18} className="text-neutral-500 cursor-pointer" />
                 </div>
                 <div className="mt-8 px-6 w-full">
                     <div className="w-full rounded-xl h-9 bg-white flex items-center px-3">
                         <p className="text-sm">Does this contract require board approval?</p>
                     </div>
+                </div>
+                <div className="px-6 mt-6 flex items-start gap-2">
+                    <LuBrain className="text-neutral-500" />
+                    <p className="text-xs text-neutral-500">Thinking through the process to find board approval requirements</p>
+                </div>
+                <div className="px-6 mt-4 flex items-start gap-2">
+                    <IoSearch className="text-neutral-500" />
+                    <div className="">
+                        <p className="text-xs text-neutral-500">Searching for mentions of board approval</p>
+                        <span className="text-[10px] px-2 py-1 bg-[#E5E5E7] rounded text-neutral-500">Section 4.2 - Board Approval Requirements</span>
+                    </div>
+                </div>
+                <div className="pr-6 pl-12 mt-4 flex items-start gap-2">
+                    <p className="text-xs text-neutral-500">Found details regarding board approval for the process of deal aquisition.</p>
+                </div>
+                <div className="pr-6 pl-12 mt-2 flex flex-col items-start gap-2">
+                    <p className="text-[10px] text-neutral-500">All contracts, agreements or commitments entered into by the Company that exceed a total value of $100,000,000  or have a term of two(2) years or longer shall require prior approval by the Board of Directors. <br /> Management may negotiate such agreements in principle.</p>
+                </div>
+                <div className="px-6 mt-6">
+                    <p className="text-xs">Based on Secton 4.2 of the project Briefs Aquisition Deal.</p>
+                </div>
+                <div className="px-6 mt-2 flex gap-3">
+                    <FiUpload className="text-neutral-500 cursor-pointer" size={14} />
+                    <PiArrowsClockwise className="text-neutral-500 cursor-pointer" size={14} />
+                    <LuCopy className="text-neutral-500 cursor-pointer" size={14} />
+                    <HiOutlineDotsHorizontal className="text-neutral-500 cursor-pointer" size={14} />
                 </div>
             </div>
         </div>
